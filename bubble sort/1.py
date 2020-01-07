731c29e5539ec5fd2d7bdc4f64352e45349c35d6
darumada <maksaturekeshov@gmail.com>
2020-01-07 22:04:14 +0600

    improve bubble sort alghoritm and add Kadane's alghoritm

--- bubble sort/1.py
+++ bubble sort/1.py
@@ -12,13 +12,18 @@
 def bubble_sort(array):
 
     for i in range(len(array)):
+        swapped = False
         for j in range(0, len(array) - i - 1):
             if array[j] > array[j + 1]:
+                swapped = True
                 array[j], array[j + 1] = array[j + 1], array[j]
+
+        if not swapped:
+            break
 
 
 arr = [9, 20, 3, 1, 8, 10, 11, 5]
 
 bubble_sort(arr)
 
-print arr  # [1, 3, 5, 8, 9, 10, 11, 20]
+print(arr)  # [1, 3, 5, 8, 9, 10, 11, 20]
---
+++ largest sum contiguous subarray/1.py
@@ -0,0 +1,32 @@
+import math
+
+'''
+
+Kadane's alghoritm для поиска подмассива с максимальной суммой
+
+@:param array: лист элементов
+
+@:return: максимальная сумма подмассива array
+
+'''
+
+
+def find_max_sub_array_sum(array):
+    local_max = 0
+    global_max = -math.inf
+
+    for i in range(len(array)):
+        local_max += array[i]
+
+        if local_max > global_max:
+            global_max = local_max
+
+        if local_max < 0:
+            local_max = 0
+
+    return global_max
+
+
+arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+
+print(find_max_sub_array_sum(arr))
